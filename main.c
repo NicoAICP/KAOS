731870b5a1ca95105a7362ca002106d568b3d791
NicoAICP <nicohaberkorn@gmx.de>
2022-06-16 02:29:37 +0200

    Button and LCD Functionality

--- main.c
+++ main.c
@@ -9,20 +9,191 @@
 #include <stdio.h>
 
 #include "pico/stdlib.h"
+
+#include "hardware/gpio.h"
+
+#include "hardware/i2c.h"
+#include "pico/binary_info.h"
+
+//////////BUTTON STUFF
+#define BUTTON_SELECT 21
+#define BUTTON_RIGHT 20
+#define BUTTON_LEFT 19
+#define BUTTON_START 18
+//////////END BUTTON
+//////////LCD STUFF
+// commands
+const int LCD_CLEARDISPLAY = 0x01;
+const int LCD_RETURNHOME = 0x02;
+const int LCD_ENTRYMODESET = 0x04;
+const int LCD_DISPLAYCONTROL = 0x08;
+const int LCD_CURSORSHIFT = 0x10;
+const int LCD_FUNCTIONSET = 0x20;
+const int LCD_SETCGRAMADDR = 0x40;
+const int LCD_SETDDRAMADDR = 0x80;
+
+// flags for display entry mode
+const int LCD_ENTRYSHIFTINCREMENT = 0x01;
+const int LCD_ENTRYLEFT = 0x02;
+
+// flags for display and cursor control
+const int LCD_BLINKON = 0x01;
+const int LCD_CURSORON = 0x02;
+const int LCD_DISPLAYON = 0x04;
+
+// flags for display and cursor shift
+const int LCD_MOVERIGHT = 0x04;
+const int LCD_DISPLAYMOVE = 0x08;
+
+// flags for function set
+const int LCD_5x10DOTS = 0x04;
+const int LCD_2LINE = 0x08;
+const int LCD_8BITMODE = 0x10;
+
+// flag for backlight control
+const int LCD_BACKLIGHT = 0x08;
+
+const int LCD_ENABLE_BIT = 0x04;
+
+// By default these LCD display drivers are on bus address 0x27
+static int addr = 0x27;
+
+// Modes for lcd_send_byte
+#define LCD_CHARACTER  1
+#define LCD_COMMAND    0
+
+#define MAX_LINES      2
+#define MAX_CHARS      16
+/* Quick helper function for single byte transfers */
+void i2c_write_byte(uint8_t val) {
+#ifdef i2c_default
+    i2c_write_blocking(i2c_default, addr, &val, 1, false);
+#endif
+}
+
+void lcd_toggle_enable(uint8_t val) {
+    // Toggle enable pin on LCD display
+    // We cannot do this too quickly or things don't work
+#define DELAY_US 600
+    sleep_us(DELAY_US);
+    i2c_write_byte(val | LCD_ENABLE_BIT);
+    sleep_us(DELAY_US);
+    i2c_write_byte(val & ~LCD_ENABLE_BIT);
+    sleep_us(DELAY_US);
+}
+
+// The display is sent a byte as two separate nibble transfers
+void lcd_send_byte(uint8_t val, int mode) {
+    uint8_t high = mode | (val & 0xF0) | LCD_BACKLIGHT;
+    uint8_t low = mode | ((val << 4) & 0xF0) | LCD_BACKLIGHT;
+
+    i2c_write_byte(high);
+    lcd_toggle_enable(high);
+    i2c_write_byte(low);
+    lcd_toggle_enable(low);
+}
+
+void lcd_clear(void) {
+    lcd_send_byte(LCD_CLEARDISPLAY, LCD_COMMAND);
+}
+
+// go to location on LCD
+void lcd_set_cursor(int line, int position) {
+    int val = (line == 0) ? 0x80 + position : 0xC0 + position;
+    lcd_send_byte(val, LCD_COMMAND);
+}
+
+static void inline lcd_char(char val) {
+    lcd_send_byte(val, LCD_CHARACTER);
+}
+
+void lcd_string(const char *s) {
+    while (*s) {
+        lcd_char(*s++);
+    }
+}
+
+void lcd_init() {
+    lcd_send_byte(0x03, LCD_COMMAND);
+    lcd_send_byte(0x03, LCD_COMMAND);
+    lcd_send_byte(0x03, LCD_COMMAND);
+    lcd_send_byte(0x02, LCD_COMMAND);
+
+    lcd_send_byte(LCD_ENTRYMODESET | LCD_ENTRYLEFT, LCD_COMMAND);
+    lcd_send_byte(LCD_FUNCTIONSET | LCD_2LINE, LCD_COMMAND);
+    lcd_send_byte(LCD_DISPLAYCONTROL | LCD_DISPLAYON, LCD_COMMAND);
+    lcd_clear();
+}
+/////////END OF LCD STUFF
+
 void hid_task(void);
 
 int main() {
+  bool start_emu = false;
+    //INIT SERIAL DEBUG
     stdio_init_all();
-    printf("USB HID Portal Emulator\n");
-    sleep_ms(500);
+    printf("INIT STDIO DONE\n");
+    sleep_ms(200);
 
+    //INIT GPIO for buttons
+    gpio_init(BUTTON_SELECT);
+    gpio_set_dir(BUTTON_SELECT, GPIO_IN);
+    gpio_pull_up(BUTTON_SELECT);
+
+    gpio_init(BUTTON_LEFT);
+    gpio_set_dir(BUTTON_LEFT, GPIO_IN);
+    gpio_pull_up(BUTTON_LEFT);
+
+    gpio_init(BUTTON_RIGHT);
+    gpio_set_dir(BUTTON_RIGHT, GPIO_IN);
+    gpio_pull_up(BUTTON_RIGHT);
+
+    gpio_init(BUTTON_START);
+    gpio_set_dir(BUTTON_START, GPIO_IN);
+    gpio_pull_up(BUTTON_START);
+
+    //INIT i2c FOR LCD
+    i2c_init(i2c_default, 100 * 1000);
+    gpio_set_function(4, GPIO_FUNC_I2C);
+    gpio_set_function(5, GPIO_FUNC_I2C);
+    gpio_pull_up(4);
+    gpio_pull_up(5);
+
+    // Make the I2C pins available to picotool
+    bi_decl(bi_2pins_with_func(4, 5, GPIO_FUNC_I2C));
+    lcd_init();
+    printf("LCD INIT done\n");
+    sleep_ms(200);
+
+    //SEND MESSAGE TO LCD
+    lcd_clear();
+    lcd_set_cursor(0, (MAX_CHARS / 2) - strlen("Skylander Portal") / 2);
+    lcd_string("Skylander Portal");
+    lcd_set_cursor(1, (MAX_CHARS / 2) - strlen("    Emulator    ") / 2);
+    lcd_string("    Emulator    ");
+
+    //INIT TINYUSB
     board_init();
     tusb_init();
     printf("INIT TinyUSB done\n");
-    sleep_ms(500);
+    sleep_ms(200);
+
     while (true) {
-        
-        tud_task(); // tinyusb device task
+
+      if(!gpio_get(BUTTON_START) && !start_emu){
+        lcd_set_cursor(1, (MAX_CHARS / 2) - strlen("Skylander Portal") / 2);
+        lcd_string("Starte Emulation");
+        printf("Starte Portal emulation");
+        sleep_ms(200);
+        start_emu = true;
+        lcd_set_cursor(1, (MAX_CHARS / 2) - strlen("    Emulator    ") / 2);
+        lcd_string("    Emuliere    ");
+      }
+
+      if(start_emu){
+        tud_task();
+      }
+         // tinyusb device task
         //sleep_ms(1000);
     }
     return 0;
